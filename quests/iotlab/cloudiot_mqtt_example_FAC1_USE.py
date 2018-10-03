5c3fba20a503f90a500f9badceb705120bd58abf
dhruvark <43543905+dhruvark@users.noreply.github.com>
2018-10-02 20:12:47 -0400

    temp under scenario

--- quests/iotlab/cloudiot_mqtt_example_FAC1_USE.py
+++ quests/iotlab/cloudiot_mqtt_example_FAC1_USE.py
@@ -267,7 +267,7 @@ def main():
             client.connect(args.mqtt_bridge_hostname, args.mqtt_bridge_port)
 
         ####### Metric Simulation###########################################
-        simulated_temp = random.uniform(70, 72)
+        simulated_temp = 69
         simulated_humidity = random.uniform(20, 30)
         simulated_pressure = random.uniform(45, 50)
         simulated_dewpoint = random.uniform(60, 70)
