f590bc91608ecc685f60f25dc1632fcebcd48302
dhruvark <43543905+dhruvark@users.noreply.github.com>
2018-10-02 21:08:00 -0400

    multiple changes towards self

--- quests/iotlab/cloudiot_mqtt_example_FAC1_USE.py
+++ quests/iotlab/cloudiot_mqtt_example_FAC1_USE.py
@@ -43,9 +43,6 @@ MAXIMUM_BACKOFF_TIME = 32
 # Whether to wait with exponential backoff before publishing.
 should_backoff = False
 
-# Define minimum temperature to be set
-temp_increase = False
-
 # [START iot_mqtt_jwt]
 def create_jwt(project_id, private_key_file, algorithm):
     """Creates a JWT (https://jwt.io) to establish an MQTT connection.
@@ -81,50 +78,57 @@ def create_jwt(project_id, private_key_file, algorithm):
     return jwt.encode(token, private_key, algorithm=algorithm)
 # [END iot_mqtt_jwt]
 
-
 # [START iot_mqtt_config]
 def error_str(rc):
     """Convert a Paho error to a human readable string."""
     return '{}: {}'.format(rc, mqtt.error_string(rc))
 
-def update_sensor_data(self):
-    """Pretend to read the device's sensor data.If the fan is on, assume the temperature decreased one degree, otherwise assume that it increased one degree."""
-    if self.increase:
-        self.mintemp = 70
-    else:
-        self.maxtemp = 72
 
-def on_connect(unused_client, unused_userdata, unused_flags, rc):
-    """Callback for when a device connects."""
-    print('on_connect', mqtt.connack_string(rc))
+class Device(object)
 
-    # After a successful connect, reset backoff time and stop backing off.
-    global should_backoff
-    global minimum_backoff_time
-    should_backoff = False
-    minimum_backoff_time = 1
+    def __init__(self):
+        self.increase = False
+        self.mintemp = 0
+        self.maxtemp = 0
 
+    def update_sensor_data(self):
+        """Pretend to read the device's sensor data.If the fan is on, assume the temperature decreased one degree, otherwise assume that it increased one degree."""
+        if self.increase:
+            self.mintemp = 70
+        else:
+            self.maxtemp = 72
 
-def on_disconnect(unused_client, unused_userdata, rc):
-    """Paho callback for when a device disconnects."""
-    print('on_disconnect', error_str(rc))
+    def on_connect(self, unused_client, unused_userdata, unused_flags, rc):
+        """Callback for when a device connects."""
+        print('on_connect', mqtt.connack_string(rc))
 
-    # Since a disconnect occurred, the next loop iteration will wait with
-    # exponential backoff.
-    global should_backoff
-    should_backoff = True
+        # After a successful connect, reset backoff time and stop backing off.
+        global should_backoff
+        global minimum_backoff_time
+        should_backoff = False
+        minimum_backoff_time = 1
 
 
-def on_publish(unused_client, unused_userdata, unused_mid):
-    """Paho callback when a message is sent to the broker."""
-    print('on_publish')
+    def on_disconnect(self, unused_client, unused_userdata, rc):
+        """Paho callback for when a device disconnects."""
+        print('on_disconnect', error_str(rc))
+
+        # Since a disconnect occurred, the next loop iteration will wait with
+        # exponential backoff.
+        global should_backoff
+        should_backoff = True
 
 
-def on_message(unused_client, unused_userdata, message):
-    """Callback when the device receives a message on a subscription."""
-    payload = str(message.payload)
-    print('Received message \'{}\' on topic \'{}\' with Qos {}'.format(
-            payload, message.topic, str(message.qos)))
+    def on_publish(self, unused_client, unused_userdata, unused_mid):
+        """Paho callback when a message is sent to the broker."""
+        print('on_publish')
+
+
+    def on_message(self, unused_client, unused_userdata, message):
+        """Callback when the device receives a message on a subscription."""
+        payload = str(message.payload)
+        print('Received message \'{}\' on topic \'{}\' with Qos {}'.format(
+                payload, message.topic, str(message.qos)))
 
 
 def get_client(
