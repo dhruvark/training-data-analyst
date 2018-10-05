3cf99527aebf7c937aff687fab3d88abbbf289a5
dhruvark <43543905+dhruvark@users.noreply.github.com>
2018-10-05 16:15:59 -0400

    update temp max

--- quests/iotlab/S1_US_EAST.py
+++ quests/iotlab/S1_US_EAST.py
@@ -263,7 +263,7 @@ def main():
 
         ####### Metric Simulation###########################################
 
-        simulated_temp = random.uniform(70, 73)
+        simulated_temp = random.uniform(70, 76)
         simulated_humidity = random.uniform(25, 30)
         simulated_pressure = random.uniform(12, 14.7)
         simulated_dewpoint = random.uniform(30, 35)
