8b9f65336e8d951191e5b66938d78a64582da45d
dhruvark <43543905+dhruvark@users.noreply.github.com>
2018-10-05 20:22:48 -0400

    JWT refresh adjusted to 2 mins

--- quests/iotlab/S1_US_EAST.py
+++ quests/iotlab/S1_US_EAST.py
@@ -210,7 +210,7 @@ def parse_command_line_args():
             help='MQTT bridge port.')
     parser.add_argument(
             '--jwt_expires_minutes',
-            default=60,
+            default=2,
             type=int,
             help=('Expiration time, in minutes, for JWT tokens.'))
 
